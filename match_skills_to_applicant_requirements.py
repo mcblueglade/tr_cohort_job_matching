e7f637bae9edc0d7f127cbd0ae74d083f3400867
mcblueglade <mchhatralia@hotmail.com>
2020-08-19 10:05:06 +0100

    Update match_skills_to_applicant_requirements.py

--- match_skills_to_applicant_requirements.py
+++ match_skills_to_applicant_requirements.py
@@ -2,6 +2,7 @@ job_skills = ['frontend', 'react', 'cloud', 'google', 'backend', 'officebased']
 applicant_requirements = ['development', 'coding', 'workfromhome', 'cloud', 'backend']
 
 def match_skills(job_skill_list, applicant_reqs_list):
+
     # Determine the total number of job_skills to match 100% for the role
     skills_len = len(job_skill_list)
 
@@ -15,16 +16,15 @@ def match_skills(job_skill_list, applicant_reqs_list):
     # Workout the score by dividing the no.of matched skills against the total job skills required
     # and mulitple by 100 to get a percentage value
 
-    #Make sure values a unique in the list
+    # Get the unique set of values in the matched skills list
+    # Make sure values are unique in the list
     matched_set = set(matched_skills)
 
-    # Get the unique set of values in the matched skills list
+    # Get the number of skills that match
     matched_skills_len = len(matched_set)
 
-    #Finally work out the score and return the value.
+    # Finally work out the score and return the value.
     return round((matched_skills_len/skills_len)*100)
 
-#Run the function
+# Run the function
 print('Score:', match_skills(job_skills, applicant_requirements))
-
-
